e8048e4c8f0e41cda029b4afa7278e42567da1af
Matt Harasymczuk <matt@astrotech.io>
2017-12-12 12:58:03 +0100

    Jenkinsfile minimal #time 3m

--- ci-cd/code/jenkinsfile-simple.groovy
+++ ci-cd/code/jenkinsfile-simple.groovy
@@ -2,8 +2,10 @@ pipeline {
     agent any
 
     stages {
-        stage("name") {
-
+        stage("simple") {
+            steps {
+                // Put your code here
+            }
         }
     }
 }
