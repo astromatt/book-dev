97d3908007173e66357d0e33b83e81bbe502a5e2
Matt Harasymczuk <matt@astrotech.io>
2017-12-12 15:52:16 +0100

    Maven #time 5m

--- ci-cd/code/jenkinsfile-maven.groovy
+++ ci-cd/code/jenkinsfile-maven.groovy
@@ -6,17 +6,9 @@ pipeline {
             steps {
 
                 // withMaven will discover the generated Maven artifacts, JUnit Surefire & FailSafe & FindBugs reports...
-                withMaven(
-                    // Maven installation declared in the Jenkins "Global Tool Configuration"
-                    maven: 'mvn_3.0.4',
-
-                    // Maven settings and global settings can also be defined in Jenkins Global Tools Configuration
-                    mavenSettingsConfig: 'my-maven-settings',
-                 ) {
-
-                      // Run the maven build with previously declared context
+                // Maven installation declared in the Jenkins "Global Tool Configuration"
+                withMaven(maven: 'mvn_3.0.4') {
                       sh "mvn clean install"
-
                  }
             }
         }
