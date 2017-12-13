87483d9c0ab47e482c225a787c29509c921856f4
Matt Harasymczuk <matt@astrotech.io>
2017-12-13 19:46:38 +0100

    Jira #time 10m

---
+++ issue-tracker/code/jira-install.sh
@@ -0,0 +1,4 @@
+wget https://www.atlassian.com/software/jira/downloads/binary/atlassian-jira-software-7.6.1-x64.bin
+chmod +x atlassian-jira-software-7.6.1-x64.bin
+./atlassian-jira-software-7.6.1-x64.bin
+rm -fr atlassian-jira-software-7.6.1-x64.bin
