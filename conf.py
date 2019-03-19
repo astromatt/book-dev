befef6f44c50c655992b34456e0a5aa6fbe7d304
Matt Harasymczuk <matt@astrotech.io>
2019-03-19 09:04:44 +0100

    Book: Config #time 10m

--- conf.py
+++ conf.py
@@ -20,9 +20,7 @@ extensions = [
 
 todo_emit_warnings = False
 todo_include_todos = True
-
 exclude_patterns = []
-
 
 # -----------------------------------------------------------------------------
 # Standard book config
@@ -35,35 +33,11 @@ import subprocess
 import sys
 
 
-numfig_format = {
-    'section': 'Sect. %s.',
-    'figure': 'Fig. %s.',
-    'table': 'Tab. %s.',
-    'code-block': 'Code Listing %s.',
-}
-
 html_theme = 'sphinx_rtd_theme'
-language = 'en'
-source_directory = '.'
-master_doc = 'index'
-highlight_language = 'python3'
-pygments_style = 'vs'
-numfig = True
-templates_path = ['_templates']
-source_suffix = ['.rst']
-imgmath_image_format = 'svg'
-today_fmt = '%Y-%m-%d'
-project_slug = re.sub(r'[\W]+', '', project)
-sha1 = subprocess.Popen('git log -1 --format="%h"', stdout=subprocess.PIPE, shell=True).stdout.read().decode().replace('\n', '')
-version = '#{sha1}, {date:%Y-%m-%d}'.format(sha1=sha1, date=datetime.date.today())
-release = '#{sha1}, {date:%Y-%m-%d}'.format(sha1=sha1, date=datetime.date.today())
-copyright = '{year}, {author} <{email}>'.format(
-    year=datetime.date.today().year,
-    author=author,
-    email=email,
-)
 
 exclude_patterns = exclude_patterns + [
+    '*/_template.rst',
+    '*/solution/*',
     '.*',
     'venv*',
     'virtualenv*',
@@ -78,17 +52,47 @@ exclude_patterns = exclude_patterns + [
     'README.rst'
 ]
 
+numfig_format = {
+    'section': 'Sect. %s.',
+    'figure': 'Fig. %s.',
+    'table': 'Tab. %s.',
+    'code-block': 'Code Listing %s.',
+}
+
+language = 'en'
+source_directory = '.'
+master_doc = 'index'
+highlight_language = 'python3'
+pygments_style = 'vs'
+numfig = True
+templates_path = ['_templates']
+source_suffix = ['.rst']
+imgmath_image_format = 'svg'
+today_fmt = '%Y-%m-%d'
+
+project_slug = re.sub(r'[\W]+', '', project)
+sha1 = subprocess.Popen('git log -1 --format="%h"', stdout=subprocess.PIPE, shell=True).stdout.read().decode().replace('\n', '')
+today = datetime.date.today()
+version = f'#{sha1}, {today:%Y-%m-%d}'
+release = f'#{sha1}, {today:%Y-%m-%d}'
+copyright = f'{today:%Y}, {author} <{email}>'
+
 extensions_dir = os.path.join(os.path.dirname(__file__), '', '_extensions')
 sys.path.append(extensions_dir)
 
+htmlhelp_basename = project
 html_theme_path = ['_themes']
 html_static_path = ['_static']
 html_favicon = '_static/favicon.png'
 html_sidebars = {'sidebar': ['localtoc.html', 'sourcelink.html', 'searchbox.html']}
 html_show_sphinx = False
-htmlhelp_basename = project
+html_context = {
+    'css_files': [
+        '_static/theme-overrides.css',
+    ],
+}
 
-latex_documents = [(master_doc, '{0}.tex'.format(project_slug), project, author, 'manual')]
+latex_documents = [(master_doc, f'{project_slug}.tex', project, author, 'manual')]
 latex_elements = {
     'papersize': 'a4paper',
     'pointsize': '10pt',
