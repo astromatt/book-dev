1fb4c2e3b99d1328fcf3197a16e4c8466f1de4d1
Matt Harasymczuk <matt@astrotech.io>
2020-04-09 08:11:09 +0200

    Book: Docker #time 1m

--- docker/image/solution/Dockerfile.java
+++ docker/image/solution/Dockerfile.java
@@ -4,4 +4,7 @@ RUN apk add git maven openjdk8
 RUN git clone https://github.com/AstroTech/ecosystem-example-java /tmp
 
 WORKDIR /tmp
-CMD mvn test
+
+# To run the unit tests, call mvn test
+# To run the integration tests as well, call mvn verify
+CMD mvn test && mvn verify
