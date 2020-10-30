27a5c437d780b1b2753abf1332d7a7e18f07171b
Matt Harasymczuk <matt@astrotech.io>
2020-10-31 00:39:29 +0100

    Book: conf #time 20m

--- conf.py
+++ conf.py
@@ -1,30 +1,24 @@
 project = 'The Software Engineering'
 author = 'Matt Harasymczuk'
-email = 'matt@astrotech.io'
+email = 'book-dev@astrotech.io'
 
+language = 'en'
 html_theme = 'sphinx_rtd_theme'
+html_baseurl = 'https://dev.astrotech.io'
 
 todo_emit_warnings = False
 todo_include_todos = True
 
 extensions = [
-    'sphinxcontrib.bibtex',
+    # 'sphinx.ext.autosectionlabel',
+    'sphinx.ext.duration',
+    # 'sphinx.ext.doctest',
+    # 'sphinx.ext.extlinks',
+    # 'sphinx.ext.graphviz',
+    # 'sphinx.ext.mathjax',
     'sphinx.ext.todo',
-]
-
-language = 'en'
-numfig_format = {
-    'section': 'Section %s.',
-    'figure': 'Figure %s.',
-    'table': 'Table %s.',
-    'code-block': 'Listing %s.',
-}
-
-exclude_patterns = [
-    '__ecosystem/**',
-    '__process/**',
-    '_layouts/**',
-    '*/_slides/**',
+    # 'sphinx.ext.viewcode',
+    'sphinxcontrib.bibtex',
 ]
 
 suppress_warnings = [
@@ -51,18 +45,25 @@ suppress_warnings = [
     # 'autosectionlabel.*',
 ]
 
-# article - for articles in scientific journals, presentations, short reports, program documentation, invitations, ...
-# proc - a class for proceedings based on the article class.
-# minimal - is as small as it can get. It only sets a page size and a base font. It is mainly used for debugging purposes.
-# report - for longer reports containing several chapters, small books, thesis, ...
-# book - for real books
-# slides - for slides. The class uses big sans serif letters.
-# memoir - for changing sensibly the output of the document. It is based on the book class, but you can create any kind of document with it (1)
-# letter - For writing letters.
-# beamer - For writing presentations (see LaTeX/Presentations).
+html_static_path = [
+    '_static',
+]
+
+# article - For articles in scientific journals, presentations, short reports,
+#           Program documentation, invitations, etc
+# proc    - A class for proceedings based on the article class.
+# minimal - Is as small as it can get. It only sets a page size and a base font.
+#           It is mainly used for debugging purposes.
+# report  - For longer reports containing several chapters, small books, thesis, etc
+# book    - For real books
+# slides  - For slides. The class uses big sans serif letters.
+# memoir  - For changing sensibly the output of the document.
+#           It is based on the book class, but you can create
+#           any kind of document with it (1)
+# letter  - For writing letters.
+# beamer  - For writing presentations (see LaTeX/Presentations).
 latex_documentclass = 'report'
 
-html_context = {}
 
 # -- Standard book config -----------------------------------------------------
 
@@ -72,7 +73,7 @@ import subprocess
 import sys
 from datetime import date
 
-needs_sphinx = '2.2'
+needs_sphinx = '3.0'
 
 imgmath_image_format = 'png'
 imgmath_latex = 'latex'
@@ -84,10 +85,16 @@ mathjax_config = {
     'jax': ['input/TeX', 'output/HTML-CSS'],
 }
 
-exclude_patterns += [
+exclude_patterns = [
+    '__ecosystem/**',
+    '__process/**',
+    '_layouts/**',
+    '*/_slides/**',
     '.*',
     'venv*',
     'virtualenv*',
+    '.venv*',
+    '.virtualenv*',
     '_build',
     '_extensions',
     '_img',
@@ -113,8 +120,11 @@ templates_path = ['_templates']
 highlight_language = 'python3'
 pygments_style = 'borland'
 autodoc_typehints = "description"
+autosectionlabel_maxdepth = 4
+
 sys.path.insert(0, os.path.abspath('_extensions'))
 
+extlinks = {'isbn': ('https://e-isbn.pl/IsbnWeb/start/search.html?szukaj_fraza=%s', 'ISBN: ')}
 
 # 0 - sequence number of image in whole document
 # 1 - sequence number of image in header level 1 (only if :numbered: option is present at toctree directive)
@@ -124,6 +134,11 @@ sys.path.insert(0, os.path.abspath('_extensions'))
 numfig_secnum_depth = 0
 numfig = True
 smartquotes = False
+numfig_format = {
+    'section': 'Section %s.',
+    'figure': 'Figure %s.',
+    'table': 'Table %s.',
+    'code-block': 'Listing %s.'}
 
 project_slug = re.sub(r'[\W]+', '', project)
 sha1 = subprocess.run('git log -1 --format="%h"', stdout=subprocess.PIPE, shell=True, encoding='utf-8').stdout.strip()
@@ -132,7 +147,7 @@ today = date.today().strftime('%Y-%m-%d')
 
 version = f'#{sha1}, {today}'
 release = f'#{sha1}, {today}'
-copyright = f'{year}, {author} <{email}>'
+copyright = f'{year}, CC-BY-SA-4.0, {author} <{email}>, last update: {today}'
 
 html_show_sphinx = False
 html_use_smartypants = False
@@ -142,32 +157,31 @@ html_theme_path = ['_themes']
 html_secnumber_suffix = '. '
 html_title = project
 html_favicon = '_static/favicon.png'
-html_static_path = ['_static']
+html_context = {}
+html_copy_source = False
 
 if html_theme == 'sphinx_rtd_theme':
     html_context.update({
-        'css_files': ['_static/screen.css', '_static/print.css'],
-        'script_files': ['_static/jquery.min.js', '_static/onload.js', mathjax_path],
-    })
+        'css_files': ['_static/light.css', '_static/print.css'],
+        'script_files': ['_static/jquery.min.js', '_static/onload.js', mathjax_path]})
 
 if html_theme == 'thesis':
     html_context.update({
         'css_files': ['_static/theme-overrides.css'],
-        'script_files': [mathjax_path],
-    })
+        'script_files': [mathjax_path]})
 
 latex_documents = [('index', f'{project_slug}.tex', project, author, latex_documentclass)]
 latex_elements = {
     'papersize': 'a4paper',
     'pointsize': '10pt',
-    'figure_align': 'htbp',
+    'figure_align': 'H',  # 'htbp',
 
     # Fix for: LaTeX Backend Fails with Citations In Figure Captions
     'preamble': r"""
+        \usepackage{float}
         \usepackage{etoolbox}
         \AtBeginEnvironment{figure}{\renewcommand{\phantomsection}{}}
-    """
-}
+    """}
 
 epub_title = project
 epub_author = author
--- docker/setup.py
+++ docker/setup.py
@@ -10,29 +10,30 @@ from subprocess import run
 
 FORMAT = 'html'
 
-sourcedir = dirname(abspath(__file__))
-project_name = basename(sourcedir)
-outputdir = join('/tmp/', project_name)
-
-rmtree(outputdir, ignore_errors=True)
-makedirs(outputdir, exist_ok=True)
-run('clear')
-
-cmd = split(f'sphinx-build -a -E -j auto --color -b {FORMAT} {sourcedir} {outputdir}')
-run(cmd)
-
-
-
 
 SECOND = 1
 MINUTE = 60 * SECOND
+START_TIME = datetime.now()
+
+sourcedir = dirname(abspath(__file__))
+project_name = basename(sourcedir)
+outdir = join('/tmp/', project_name)
+rmtree(outdir, ignore_errors=True)
+makedirs(outdir, exist_ok=True)
+run('clear')
+cmd = split(f'sphinx-build -a -E -j auto --color -b {FORMAT} {sourcedir} {outdir}')
+run(cmd)
 
 last = run('git log -1 --format="%ad" --date=iso', shell=True, capture_output=True).stdout.strip().decode()
 last = datetime.strptime(last, '%Y-%m-%d %H:%M:%S %z')
-print('Last commit:', last)
-
 delta = datetime.now(tz=timezone.utc) - last
-min = delta.total_seconds() / MINUTE
-min = round(min)
+since = round(delta.total_seconds() / MINUTE)
 
-print(f'Since: {min}m')
+duration = datetime.now() - START_TIME
+duration_seconds = round(duration.total_seconds())
+duration_minutes = round(duration_seconds / MINUTE, 1)
+
+print(f'\n\n')
+print(f'Build took: {duration_seconds} seconds ({duration_minutes} minutes)')
+print(f'Last commit: {last}')
+print(f'Since: {since}m')
