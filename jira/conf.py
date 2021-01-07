20f21dc5bd8b68b6d2929beeae7412adb4e4b19c
Matt Harasymczuk <matt@astrotech.io>
2021-01-08 00:33:28 +0100

    Book: Jira #time 2m

--- jira/conf.py
+++ jira/conf.py
@@ -1,5 +1,5 @@
 import sys; sys.path.append('..')
-from ..conf import *
+from conf import *
 
 project = 'Jira with Atlassian Ecosystem'
 author = 'Matt Harasymczuk'
@@ -11,3 +11,6 @@ todo_include_todos = True
 
 html_favicon = '../_static/favicon.png'
 html_static_path = ['../_static']
+html_context.update({
+        'css_files': ['_static/light.css', '_static/print.css'],
+        'script_files': ['_static/jquery.min.js', '_static/onload.js']})
